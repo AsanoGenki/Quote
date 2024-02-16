8dadf3f5abeea79bfd9571c3cd52ee401be36c7b
AsanoGenki <asanogenki0218@icloud.com>
2024-02-16 21:48:48 +0900

    Initial commit.

---
+++ Quote/QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteApp.swift
+//  Quote
+//
+//  Created by Genki on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
